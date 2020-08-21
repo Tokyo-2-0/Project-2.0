110cd98ab74f88d0bbec457d10f572d491e6f831
Nadim-Barkati <barkatinadim1994@gmail.com>
2020-08-21 17:09:49 +0100

    musicplayer and history pages added

---
+++ js/signIn.js
@@ -0,0 +1,12 @@
+var user1 =['Whigga','halloworld@'];
+var user2=['slipknot666','azerty123'];
+$(document).ready(function() {
+    $('#click').on('click', function() {
+        if (#loger1===user1[0]&&#loger2===user1[1]){
+            alert 'Welcome '+user1[0];
+        }
+        else if(#loger1===user2[0]&&#loger2===user2[1]){
+            alert "Welcome "+user2[0];
+        }
+    });
+})
